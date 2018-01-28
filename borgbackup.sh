83dad8ade5a71a743e38f6fe76b148cfe3b40167
Alexander Holzapfel <alex@huizi.at>
2018-01-28 17:58:24 +0100

    addet config file and options to disable or enable mailgun

--- borgbackup.sh
+++ borgbackup.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 
 ##
-## Variablen
+## load config file
 ##
 
-LOG="" # path of logfile
-BACKUP_USER="" # username for repository
-REPOSITORY_DIR="" # remote directory to store backups
-RSA_PATH="" # path of rsa key
-PASSPHRASE="" # passphrase for encryption of borg backup
-BNAME={now:%Y-%m-%d_%H:%M} # name of backup
+if [[ -z "$1" ]]; then
+    CONFIG_FILE="$(dirname $0)/local.conf"
+    echo "Using $CONFIG_FILE as config."
+else
+    CONFIG_FILE=$1
+fi
+
+if ! source "$(dirname $0)/local.conf"; then
+    echo "Error: can't load configuration file local.conf"
+    exit 1
+fi
+
+# only overwrite defaults
+if ! source "$CONFIG_FILE"; then
+    echo "Error: can't load configuration file $CONFIG_FILE"
+    exit 1
+fi
 
 ##
-## Repository
-##
-
-REPOSITORY="ssh://${BACKUP_USER}@${BACKUP_USER}.your-storagebox.de:23/./${REPOSITORY_DIR}" # repository address, in example with hetzner storage box
-
-##
-## Ausgabe in Logfile
+## write logfile
 ##
 
 exec > >(tee -i ${LOG})
@@ -27,20 +32,13 @@ echo "##### Hostname: $(hostname) #####"
 echo "##### Backup gestartet $(date) #####"
 
 ##
-## Borg Export
-##
-
-export BORG_PASSPHRASE="$PASSPHRASE"
-export BORG_RSH="ssh -i $RSA_PATH"
-
-##
 ## BORG BACKUP
 ##
 
 echo "Synchronisiere Dateien ..."
-borg create -v --stats -C lz4                           \
-    $REPOSITORY::${BNAME}                               \
-      /                                                 \
+borg create -v --one-file-system --stats -C lz4            \
+	::${PREFIX}-{now:%Y-%m-%d_%H:%M}                    \
+      $BACKUP_DIRS                                      \
       --exclude /dev                                    \
       --exclude /proc                                   \
       --exclude /sys                                    \
@@ -48,36 +46,41 @@ borg create -v --stats -C lz4                           \
       --exclude /run                                    \
       --exclude /lost+found                             \
       --exclude /mnt                                    \
-      --exclude /var/lib/lxcfs
+      --exclude /var/lib/lxcfs				            \
+      --exclude /lib/modules				            \
+      --exclude '*.pyc'                           	    \
+      --exclude '/var/backups/'                         \
+      --exclude '/var/log/'                           	\
+      --exclude '*.swp'
 
 ##
 ## BORG PRUNE
 ##
 
-borg prune -v --list $REPOSITORY \
-    --keep-daily=7 --keep-weekly=4 --keep-monthly=6
+borg prune -v -s --save-space --list :: --prefix ${PREFIX}- $KEEP_BACKUPS
 
 ##
-## Backup beenden
+## stop backup
 ##
 
 echo "###### Backup beendet: $(date) ######"
 
 ##
-## Mail versenden
-## send mails with the mailgun curl "api"
+## send mails with the mailgun api
 ## https://documentation.mailgun.com/en/latest/quickstart-sending.html#send-via-api
 ##
 
-curl -s --user '#####' \ # place your api key instead of #####
-    https://api.mailgun.net/v3/#####/messages \ # place your domain name instead of #####
-    -F from='AH:Backup <backup@example.tld>' \ # sender of the mail
-    -F to=### \ # recipient of the mail
-    -F subject="AH:Backup on $(hostname -f)" \ # subject of the mail
-    -F text="$(cat $LOG)"
+if [[ $MGENABLE = 1 ]]; then
+    curl -s --user $MGAPI \
+        https://api.mailgun.net/v3/$MGDOMAIN/messages \
+        -F from="AH:Backup <$MGMAIL>" \
+        -F to=$MGRECIPIENT \
+        -F subject="AH:Backup on $(hostname -f)" \
+        -F text="$(cat $LOG)"
+fi
 
 ##
-## Log schreiben und löschen
+## Write log and delete temporary log
 ##
 
 echo "" >> /var/log/borg.log
